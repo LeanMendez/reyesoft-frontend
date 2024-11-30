72a1b33df68b32afb4fee269c5c0f22463a93742
Leandro Mendez <leandroa.mendez@gmail.com>
2024-11-30 14:23:51 -0300

    chore: setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'reyesoft-challenge-angular';
+  title = 'Challenge Angular';
 }
